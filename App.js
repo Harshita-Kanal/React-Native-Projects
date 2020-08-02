a704168ce0b4ca5c52c79a35411716375a8105fc
Harshita-Kanal <harshita.kgv@gmail.com>
2020-08-03 03:35:32 +0530

    ListView

--- App.js
+++ App.js
@@ -1,22 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import Main from './components/MainComponent'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Main />
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#675690',
-    alignItems: 'center',
-    justifyContent: 'center',
-    fontFamily: 'sans-serif'
-  },
-});
+
+
